3086a73bd54babe59e5431c9674b72a3940fb84e
Son Ta Hoai <son.ta@d-net.co.za>
2025-12-01 10:21:21 +0700

    add order entities

---
+++ src/LegacyOrderService.Domain/Entities/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LegacyOrderService.Domain.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string? CustomerName { get; set; }
+        public string? ProductName { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+    }
+}
